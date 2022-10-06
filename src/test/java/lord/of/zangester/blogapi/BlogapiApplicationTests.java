9607115c1c5bae0723f2293ac33c689690b9f8cb
Muhammad Rifqi Alfauzan <rifqialfauzan03@gmail.com>
2022-10-06 11:17:06 +0700

    initial commit

---
+++ src/test/java/lord/of/zangester/blogapi/BlogapiApplicationTests.java
@@ -0,0 +1,13 @@
+package lord.of.zangester.blogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
